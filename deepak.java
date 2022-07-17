a6d143c6bdd0fddca43ac8cadae50b08b9432d1f
sujith <gongati.sujithreddy2004@gmail.com>
2022-07-17 23:13:51 +0530

    first push

---
+++ deepak.java
@@ -0,0 +1,5 @@
+public class deepak {
+	public static void main() [D[DString args[]) {
+		System.out.print("Hello, world!");[D[D[D[D[D[D[D[D[[[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D
+	}
+}
---
+++ suji.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main() {
+}[D
+[A[	printf("Sujith");
+}
